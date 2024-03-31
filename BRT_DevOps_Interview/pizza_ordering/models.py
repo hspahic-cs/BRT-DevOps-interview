7d621b98360a7fbbba418a6eee17b5c44defffab
harris <harrisspahic1190@gmail.com>
2024-03-31 12:02:58 -0400

    Adding model for Order

--- BRT_DevOps_Interview/pizza_ordering/models.py
+++ BRT_DevOps_Interview/pizza_ordering/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    # Defining constants for the pizza sizes
+    TOPPING_CHOICES = [
+        ('P', 'Pepperoni'),
+        ('M', 'Mushrooms'),
+        ('O', 'Onions'),
+        ('S', 'Sausage'),
+        ('B', 'Bacon'),
+        ('G', 'Extra cheese'),
+        ('', 'None')
+    ]
+    
+    # Fields
+    comment = models.TextField(max_length=300, blank=True)
+    toppings = models.CharField(max_length=1, 
+                                choices=TOPPING_CHOICES, 
+                                default='')
+    
+    
