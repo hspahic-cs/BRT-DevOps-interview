43198d58f944b1582485dc1dbee86ddbcf1ab1cc
harris <harrisspahic1190@gmail.com>
2024-04-02 03:05:51 -0400

    Added README.md with deployment instructions & cleaned code a bit

--- kubernetes/deployment_helper.sh
+++ kubernetes/deployment_helper.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+
+# ====== DO NOT RUN BASH SCRIPT, ORDER PROVIDED IN README ======
+
+# Apply configuration maps and secrets
 kubectl apply -f application/configmap.yaml
 kubectl apply -f application/secret.yaml
 kubectl apply -f database/secret.yaml
+sleep 2  # Pause for 2 seconds
 
+# Apply storage configurations
 kubectl apply -f database/storage.yaml
 kubectl apply -f application/storage.yaml
+sleep 2  # Pause for 2 seconds
 
+# Apply services
 kubectl apply -f database/service.yaml
 kubectl apply -f application/service.yaml
-# Repeat to apply service ip
+# Repeat to apply service IP
 kubectl apply -f application/configmap.yaml
+sleep 2  # Pause for 2 seconds
 
+# Deploy database and application
 kubectl apply -f database/deployment.yaml
 kubectl apply -f application/deployment.yaml
+sleep 2  # Pause for 2 seconds
 
+# Apply jobs
 kubectl apply -f application/jobs.yaml
