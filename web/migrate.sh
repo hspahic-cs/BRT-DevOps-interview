b93b2d807d09728ff9485eaa0327a67d5419fb3a
harris <harrisspahic1190@gmail.com>
2024-04-02 01:45:30 -0400

    Added automatic superuser creation, and database migration via jobs

--- web/migrate.sh
+++ web/migrate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"test@email.com"}
 cd /app/
 
 
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
-cd ..
